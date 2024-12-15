e45ee68dd16b6a77bb4326e4c7be5ebf466831ba
akii <158286814+helloankitpandey@users.noreply.github.com>
2024-12-15 21:59:51 +0530

    change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,8 +15,6 @@ const App = () => {
   useEffect(() => {
     const fetchContacts = async () => {
       try {
-        console.log("Chnage");
-
         // here it is hosted on localhost
         const response = await axios.get('http://localhost:3000/api/contacts');
         setContacts(response.data);
